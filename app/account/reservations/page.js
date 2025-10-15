5e9d0efb376cf65565d7253afda1387fda95942b
hectormc2189 <140911666+hectormc2189@users.noreply.github.com>
2025-10-15 14:57:55 -0400

    Fix import case for ReservationList component

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,4 +1,4 @@
-import ReservationList from "@/app/_components/reservationList";
+import ReservationList from "@/app/_components/ReservationList";
 import { auth } from "@/app/_lib/auth";
 import { getBookings } from "@/app/_lib/data-service";
 import Link from "next/link";
